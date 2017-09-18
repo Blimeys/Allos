dfb43cc9bdb52c4d636a691ab68a23e75a71c609
Blimeys <djeanmath@gmail.com>
2017-09-18 22:20:54 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 const app = express();
+
+require('./routes/authRoutes')(app);
+mongoose.connect(keys.mongoURI);
+
+app.use(bodyParser.json());
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
 	res.send('Hello');
@@ -8,7 +25,14 @@ app.get('/', (req, res) => {
 app.get('/api/', (req, res) => {
 	res.send('Api loc');
 });
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+	});
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
