152cb11dec082ed8fb35920d2deac1e28bc8d2d1
Blimeys <djeanmath@gmail.com>
2017-10-07 21:21:50 +0100

    filter with state kind of working

--- client/src/components/menus/MenusList.js
+++ client/src/components/menus/MenusList.js
@@ -2,30 +2,60 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { fetchMenus, deleteMenuItem } from '../../actions';
+
 // import LocationList from './LocationList';
 
 class MenusList extends Component {
+	constructor() {
+		super();
+		// const allergies = {
+		// 	gluten:false,
+		// 	crustacean: false,
+		// 	egg: false,
+		// 	fish: false,
+		// 	peanut: false,
+		// 	soybean: false,
+		// 	milk: false,
+		// 	nuts: false,
+		// 	celery: false,
+		// 	mustard: false,
+		// 	sesame: false,
+		// 	sulphite: false,
+		// 	lupin: false,
+		// 	mollusc: false
+		// }
+
+		this.state = { location: false, category: false };
+	}
 	componentDidMount() {
 		this.props.fetchMenus();
 		// const allergens = [
-		// 	'gluten',
-		// 	'crustacean',
-		// 	'egg',
-		// 	'fish',
-		// 	'peanut',
-		// 	'soybean',
-		// 	'milk',
-		// 	'nuts',
-		// 	'celery',
-		// 	'mustard',
-		// 	'sesame',
-		// 	'sulphite',
-		// 	'lupin',
-		// 	'mollusc'
+		// 'gluten',
+		// 'crustacean',
+		// 'egg',
+		// 'fish',
+		// 'peanut',
+		// 'soybean',
+		// 'milk',
+		// 'nuts',
+		// 'celery',
+		// 'mustard',
+		// 'sesame',
+		// 'sulphite',
+		// 'lupin',
+		// 'mollusc'
 		// ];
 	}
 	componentWillUpdate() {
 		this.props.fetchMenus();
+	}
+	handleLocation(location) {
+		this.setState({ location: location });
+		console.log(this.state.location);
+	}
+	handleCategory(category) {
+		this.setState({ category: category });
+		console.log(this.state.category);
 	}
 	renderCategories() {
 		const categories = [];
@@ -35,7 +65,11 @@ class MenusList extends Component {
 		var newCategories = [...new Set(categories)];
 		return newCategories.map(category => {
 			return (
-				<button key={category} if={category}>
+				<button
+					key={category}
+					if={category}
+					onClick={this.handleCategory.bind(this, category)}
+				>
 					{category}
 				</button>
 			);
@@ -49,7 +83,11 @@ class MenusList extends Component {
 		var newLocations = [...new Set(locations)];
 		return newLocations.map(location => {
 			return (
-				<button key={location} id={location}>
+				<button
+					key={location}
+					id={location}
+					onClick={this.handleLocation.bind(this, location)}
+				>
 					{location}
 				</button>
 			);
@@ -58,39 +96,41 @@ class MenusList extends Component {
 
 	renderMenus() {
 		return this.props.menus.reverse().map(menu => {
-			return (
-				<div key={menu._id} id={(menu.location, menu.category)}>
-					<div>
-						<h2>
-							{menu.title}
-						</h2>
-						<p>
-							{menu.description}
-						</p>
-						<ul>
-							{menu.nuts ? <li>nuts</li> : null}
-							{menu.gluten ? <li>gluten</li> : null}
-							{menu.crustacean ? <li>crustacean</li> : null}
-							{menu.egg ? <li>egg</li> : null}
-							{menu.fish ? <li>fish</li> : null}
-							{menu.peanut ? <li>peanut</li> : null}
-							{menu.soybean ? <li>soybean</li> : null}
-							{menu.milk ? <li>milk</li> : null}
-							{menu.nuts ? <li>nuts</li> : null}
-							{menu.celery ? <li>mustard</li> : null}
-							{menu.sesame ? <li>sesame</li> : null}
-							{menu.sulphite ? <li>sulphite</li> : null}
-							{menu.lupin ? <li>lupin</li> : null}
-							{menu.mollusc ? <li>mollusc</li> : null}
-						</ul>
-					</div>
-					<button onClick={deleteMenuItem(menu)}>delete</button>
-					{/* Implement edit */}
-					{/* <Link to="/menus/edit">
+			if (menu.location === this.state.location) {
+				return (
+					<div key={menu._id} id={(menu.location, menu.category)}>
+						<div>
+							<h2>
+								{menu.title}
+							</h2>
+							<p>
+								{menu.description}
+							</p>
+							<ul>
+								{menu.gluten ? <li>gluten</li> : null}
+								{menu.crustacean ? <li>crustacean</li> : null}
+								{menu.egg ? <li>egg</li> : null}
+								{menu.fish ? <li>fish</li> : null}
+								{menu.peanut ? <li>peanut</li> : null}
+								{menu.soybean ? <li>soybean</li> : null}
+								{menu.milk ? <li>milk</li> : null}
+								{menu.nuts ? <li>nuts</li> : null}
+								{menu.celery ? <li>mustard</li> : null}
+								{menu.sesame ? <li>sesame</li> : null}
+								{menu.sulphite ? <li>sulphite</li> : null}
+								{menu.lupin ? <li>lupin</li> : null}
+								{menu.mollusc ? <li>mollusc</li> : null}
+							</ul>
+						</div>
+						<button onClick={deleteMenuItem(menu)}>delete</button>
+						{/* Implement edit */}
+						{/* <Link to="/menus/edit">
 						<button>Edit</button>
 					</Link> */}
-				</div>
-			);
+					</div>
+				);
+			}
+			return null;
 		});
 	}
 	render() {
@@ -103,7 +143,7 @@ class MenusList extends Component {
 				<h1>Categories</h1>
 				{this.renderCategories()}
 				<h1>Menus Items</h1>
-				{this.renderMenus()}
+				{this.renderMenus(this.state.location)}
 			</div>
 		);
 	}
